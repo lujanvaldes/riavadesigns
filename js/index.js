e7e07343970d3c366493e70aa8c5f3f059e1691b
lujanvaldes <marialujanvaldes@alumnos.cei.es>
2025-01-08 18:21:27 +0100

    .

---
+++ js/index.js
@@ -0,0 +1,49 @@
+//   0. Comentarios y documentación   //
+
+/*
+En este js se presentan las funciones y eventos necesarios para incluir contenidos
+y cambiar las apariencias del index.html.
+
+Se utiliza para agregar interactividad y mejorar la experiencia
+que tenga el usuario en la web.
+*/
+
+
+//   1. Constantes y Variables   //
+
+/// constantes
+
+
+
+
+
+/// variables
+
+
+
+
+//   2. Funciones utilitarias (reutilizables)   //
+
+
+
+
+//   3. Funciones principales de la web   //
+
+
+
+
+//   4. eventListeners   //
+
+
+
+//   5. Inicializar el programa   //
+
+
+
+
+
+
+
+
+
+
---
+++ js/menu.js
@@ -0,0 +1,39 @@
+//   0. Comentarios y documentación   //
+
+/*
+En este js se presentan las funciones y eventos necesarios para incluir contenidos
+y cambiar las apariencias del menu.html.
+
+Se añaden la interactividad que requiere para que el objetivo del menu
+(mostrar las secciones que posee toda la web).
+*/
+
+
+//   1. Constantes y Variables   //
+
+/// constantes
+
+
+
+
+
+/// variables
+
+
+
+
+//   2. Funciones utilitarias (reutilizables)   //
+
+
+
+
+//   3. Funciones principales de la web   //
+
+
+
+
+//   4. eventListeners   //
+
+
+
+//   5. Inicializar el programa   //
